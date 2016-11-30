a85eeaa2cae89211443cdee038f5b91a2f5e0575
dalbergiaretusa <bastianksiazczyk@gmail.com>
2016-11-30 16:09:15 +0000

    added splash screen

--- floodit.rb
+++ floodit.rb
@@ -1,3 +1,9 @@
+#Flood It - Sebastian Ksiazczyk, 2016
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default)
+
 
 def get_board(width, height)
   # TODO: Implement this method
@@ -17,6 +23,22 @@ def get_board(width, height)
   # this will be used for checking the functionality
   # of your implementation.
 end
+def change_board_size
 
-# TODO: Implement everything else as described in the
-#       assignment brief.
+end
+
+# SPLASH SCREEN
+splash = ConsoleSplash.new(20,60)
+
+splash.write_left_pattern("0", {:fg=>:cyan})
+splash.write_right_pattern("0", {:fg=>:light_cyan})
+splash.write_bottom_pattern("*-..-*", {:fg=>:cyan})
+splash.write_top_pattern(".-**-.", {:fg=>:light_cyan})
+
+splash.write_header("Flood-It", "Sebastian Ksiazczyk", "0.1.0", 
+        {:nameFg=>:light_magenta, :authorFg=>:light_green, :versionFg=>:yellow})
+splash.write_center(-5, "*press [ENTER] to continue*", :fg=>:light_magenta)
+
+splash.splash
+# END OF SPLASH SCREEN 
+
