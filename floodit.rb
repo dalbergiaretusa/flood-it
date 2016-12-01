c4a2938fd1ab029db8d514004674fc1e4736d01f
dalbergiaretusa <bastianksiazczyk@gmail.com>
2016-12-01 09:16:14 +0000

    Added main menu, added change_board_size

--- floodit.rb
+++ floodit.rb
@@ -4,6 +4,8 @@ require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
+$width=14
+$length=9
 
 def get_board(width, height)
   # TODO: Implement this method
@@ -23,10 +25,20 @@ def get_board(width, height)
   # this will be used for checking the functionality
   # of your implementation.
 end
-def change_board_size
 
+def change_board_size
+	puts "Current board size: #{$width} x #{$length}"
+	puts "Please enter your desired table size below:"
+	print "Width:"
+	$width=gets.chomp.to_i
+	print "Length:"
+	$length=gets.chomp.to_i
+	puts "The board size has been changed to #{$width} x #{$length}."
 end
 
+def play_game(array)
+
+end
 # SPLASH SCREEN
 splash = ConsoleSplash.new(20,60)
 
@@ -42,3 +54,34 @@ splash.write_center(-5, "*press [ENTER] to continue*", :fg=>:light_magenta)
 splash.splash
 # END OF SPLASH SCREEN 
 
+# MAIN MENU
+menu = []
+menu << "Main Menu"
+menu << "s: Start game"
+menu << "c: Change board size"
+menu << "q: Quit game"
+menu << "No games played yet"
+
+input=gets
+
+if input=="\n"
+	while true do
+		puts menu
+		puts "Please enter your choice:"
+		input=gets.chomp
+		if input=='q'
+			puts "It was lovely to meet you. See you later!"
+			exit
+		elsif input=='c'
+			change_board_size
+		elsif input=='s'
+			play_game(get_board($width, $height))
+		else
+			puts "Undefined input. Please try again"
+		end
+	end
+		
+	end
+
+# END OF MAIN MENU
+# MAIN LOOP
